353bcc21dc90f87cc9240ee2956cc629b6a76e03
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-11 07:53:25 +0600

    hide firebase config with env variables

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import About from '../About/About';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
-            <p>Home page</p>
+            <Banner/>
+            <About/>
+            <Services/>
         </div>
     );
 };
